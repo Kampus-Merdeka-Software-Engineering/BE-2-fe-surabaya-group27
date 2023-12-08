3c5b55feae1c53a72d944139882b2e26cc3047a7
Th0m-as <veronikatamoinna0007@gmail.com>
2023-12-09 01:51:15 +0700

    test

--- routes/newsroutes.js
+++ routes/newsroutes.js
@@ -1,36 +1,9 @@
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
-
 const router = express.Router();
-const prisma = new PrismaClient();
 
-// GET all books
-router.get('/news', async (req, res) => {
-  try {
-    const books = await prisma.News.findMany();
-    res.json({ success: true, data: books });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-});
+const bookController = require('../controllers/newscontrollers');
 
-// POST a new book
-router.post('/news', async (req, res) => {
-  const { title, author, publishedDate } = req.body;
-
-  try {
-    const newBook = await prisma.players.create({
-      data: {
-        title,
-        author,
-        publishedDate,
-      },
-    });
-
-    res.json({ success: true, data: newBook });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-});
+router.get('/books', bookController.getAllBooks);
+router.post('/books', bookController.createBook);
 
 module.exports = router;
